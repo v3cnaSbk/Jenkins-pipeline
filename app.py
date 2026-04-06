7087a809e7424427c36a14b96b25f74873f56328
v3cnaSbk <dhiraj.israel@gmail.com>
2026-04-06 16:24:17 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# A dictionary to store tasks with an ID
+# A dictionary to store tasks with an ID and Description
 tasks = {}
 task_id_counter = 1
 
